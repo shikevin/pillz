3397f81db126c16664f580f2e3f2eca4ef631fa5
Allen Wang <allen.houze.wang@gmail.com>
2015-09-20 03:07:53 -0400

    upgraded image detectin

--- image-recognition/counting_objects.py
+++ image-recognition/counting_objects.py
@@ -3,50 +3,109 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+def get_mask_bounds(mask):
+
+    top = len(mask)
+    right = 0
+    bottom = 0
+    left = len(mask[0])
+
+    for row in mask:
+        for column in mask[row]: 
+            print mask[row][column]
+            if mask[row][column] == (255, 255, 255):
+                if top < row:
+                    top = row
+                if column > right:
+                    right = column
+                if bottom < row:
+                    bottom = row
+                if left > column:
+                    left = column
+
+    return top, right, bottom, left
+
+
+def water_shed_object_counting(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    ret, thresh = cv2.threshold(gray,220,255,cv2.THRESH_BINARY)
+
+    # noise removal
+    kernel = np.ones((5,5),np.uint8)
+    opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+
+    # sure background area
+    sure_bg = cv2.dilate(opening,kernel,iterations=3)
+
+
+    # Finding sure foreground area
+    dist_transform = cv2.distanceTransform(opening,cv2.cv.CV_DIST_L2,5)
+    ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+
+    # Finding unknown region
+    sure_fg = np.uint8(sure_fg)
+
+    contours, hierarchy = \
+        cv2.findContours(sure_fg,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+    return len(contours)
+
 # 
 # modify this to take another image
 #
-img = cv2.imread('water_coins.jpg')
-#
-#
+img = cv2.imread('test1.JPG')
+
+# to be tweaked around
+colour_boundaries = {
+    'red': ([30, 30, 130], [130, 130, 255]),
+    'blue': ([130, 30, 30], [255, 130, 130]),
+    'green': ([0, 90, 0], [100, 210, 100])
+}
 
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+pill_counts = {
+    'red': 0,
+    'blue': 0,
+    'green': 0,
+}
+
+for colour in colour_boundaries:
+
+    # create NumPy arrays from the boundaries
+    lower = np.array(colour_boundaries[colour][0], dtype = "uint8")
+    upper = np.array(colour_boundaries[colour][1], dtype = "uint8")
+
+    # print colour
+    # print colour_boundaries
+    # print colour_boundaries[colour]
+    # print colour_boundaries[colour][0]
+    # print colour_boundaries[colour][1]
+
+    # print lower
+    # print upper
+    
+ 
+    # find the colors within the specified boundaries and apply the mask
+    colour_mask = cv2.inRange(img, lower, upper)
+
+    
+
+    # clean the noises
+    kernel = np.ones((80,80),np.uint8)
+    colour_mask_cleaned = cv2.morphologyEx(colour_mask, cv2.MORPH_CLOSE, kernel)
+
+    colour_region = cv2.bitwise_and(img, img, mask = colour_mask_cleaned)
 
 
-cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
-# noise removal
-kernel = np.ones((3,3),np.uint8)
-opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
-
-# sure background area
-sure_bg = cv2.dilate(opening,kernel,iterations=3)
 
 
-# Finding sure foreground area
-dist_transform = cv2.distanceTransform(opening,cv2.cv.CV_DIST_L2,5)
-ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
-
-# Finding unknown region
-sure_fg = np.uint8(sure_fg)
+    pill_counts[colour] = water_shed_object_counting(colour_region)
 
 
 
-# plt.imshow(sure_fg, cmap='gray', interpolation='bicubic')
-# plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
-# plt.show()
-
-contours, hierarchy = \
-    cv2.findContours(sure_fg,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-print len(contours)
+print pill_counts
 
 
 
-
-
-# plt.imshow(img, cmap='gray', interpolation='bicubic')
-# plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
-# plt.show()
+# cv2.destroyAllWindows()
